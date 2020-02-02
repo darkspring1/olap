496a498e5f5b4fbd6e31eccea1ac4851a7342940
Olt Egor <e.olt@softpro.com>
2020-02-02 18:03:12 +0300

    react redux routing

--- front/src/index.js
+++ front/src/index.js
@@ -5,20 +5,32 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers'
 import UpdateModelDescriptionSaga from './sagas'
+import { Router, Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
 import App from './app'
+
 
 const sagaMiddleware = createSagaMiddleware()
 
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
-
+const history = syncHistoryWithStore(createBrowserHistory(), store);
 // затем запускаем saga
 sagaMiddleware.run(UpdateModelDescriptionSaga)
 
-//store.runSaga(UpdateModelDescriptionSaga)
-
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}/>
+      {/* <Router path="/about" component={About}/> */}
+    </Router>
   </Provider>,
-  document.querySelector("#root")
-)
+  document.getElementById('root')
+);
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.querySelector("#root")
+// )
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux';
+
 import excelGrid from './excelGrid'
 import empty from "./empty"
 
 export default combineReducers({
+  routing: routerReducer,
   excelGrid,
   empty
 })
