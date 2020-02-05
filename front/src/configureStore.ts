023829137a83622c5669590434fe0f0a7a4999b8
Olt Egor <e.olt@softpro.com>
2020-02-05 22:39:49 +0300

    redux dev tool

--- front/src/configureStore.ts
+++ front/src/configureStore.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
@@ -7,13 +8,15 @@ import createRootReducer from './reducers/index.ts';
 
 export const history = createBrowserHistory();
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore(preloadedState: any) {
   const sagaMiddleware = createSagaMiddleware();
 
   const store = createStore(
     createRootReducer(history), // root reducer with router state
     preloadedState,
-    compose(
+    composeEnhancers(
       applyMiddleware(
         routerMiddleware(history), // for dispatching history actions
         sagaMiddleware,
