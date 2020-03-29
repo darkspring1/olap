e3f366092df39f4240e3699b9a0f6c913f77c0c8
Egor Olt <>
2020-03-29 23:44:21 +0300

    tests

---
+++ front/tests/test.js
@@ -0,0 +1,7 @@
+// import { expect } from 'chai';
+
+describe('distance', () => {
+  it("calculates distance with the good ol' Pythagorean Theorem", () => {
+    // expect(1).to.equal(5);
+  });
+});
